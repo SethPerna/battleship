9d78f4ea607a737df56d6c4a2a644312caf1e2a5
Eric Chon <echon92@gmail.com>
2021-10-18 20:40:32 -0600

    Added instance of ship and tested instance

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -5,7 +5,6 @@ require './lib/cell'
 RSpec.describe Cell do
   before :each do
     @cell = Cell.new("B4")
-    @cruiser = Ship.new("Cruiser", 3)
   end
 
   it "exists" do
@@ -21,5 +20,11 @@ RSpec.describe Cell do
     expect(@cell.empty?).to be true
   end
 
-  
+  before :each do
+      @cruiser = Ship.new("Cruiser", 3)
+    end
+
+      it "checks if a ship exists" do
+        expect(@cruiser).to be_an_instance_of(Ship)
+      end
 end
