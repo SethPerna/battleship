788dae98d35eca11d1491961c56d6c8369ba907d
Seth Perna <gseth26@gmail.com>
2021-10-18 21:51:27 -0400

    finish ship class and test

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -13,14 +13,25 @@ RSpec.describe Ship do
   it "returns health" do
     expect(@cruiser.health).to eq(3)
   end
-  it "checks if ship is sunk" do
+  it "checks if ship is #sunk" do
     expect(@cruiser.sunk?).to be false
   end
-  it "lowers health when hit" do
+  it "lowers health when #hit" do
     expect(@cruiser.hit).to eq(2)
   end
   it "lowers health again" do
     @cruiser.hit
     expect(@cruiser.hit).to eq(1)
+  end
+  it "check if ship #sunk is again" do
+    @cruiser.hit
+    @cruiser.hit
+    expect(@cruiser.sunk?).to be false
+  end
+  it "returns ship is #sunk" do
+    @cruiser.hit
+    @cruiser.hit
+    @cruiser.hit
+    expect(@cruiser.sunk?).to be true
   end
 end
