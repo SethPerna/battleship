8e80faca49355efdfb42a5c3afe8e2772170ea6e
Seth Perna <gseth26@gmail.com>
2021-10-26 15:47:33 -0400

    clean up indentation for cell_spec

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -22,97 +22,98 @@ RSpec.describe Cell do
 
   before :each do
     @cruiser = Ship.new("Cruiser", 3)
-    end
+  end
 
-      it "checks if a ship exists" do
-        expect(@cruiser).to be_an_instance_of(Ship)
-      end
+  it "checks if a ship exists" do
+    expect(@cruiser).to be_an_instance_of(Ship)
+  end
 
-      it "checks if a ship is placed" do
-        @cell.place_ship(@cruiser)
-        expect(@cell.ship).to eq(@cruiser)
-      end
+  it "checks if a ship is placed" do
+    @cell.place_ship(@cruiser)
+    expect(@cell.ship).to eq(@cruiser)
+  end
 
-      it "checks if coordinate is occupied" do
-        @cell.place_ship(@cruiser)
-        expect(@cell.empty?).to be false
-      end
+  it "checks if coordinate is occupied" do
+    @cell.place_ship(@cruiser)
+    expect(@cell.empty?).to be false
+  end
 
-      it "checks if fired upon" do
-        @cell.place_ship(@cruiser)
-        expect(@cell.fired_upon?).to be false
-      end
+  it "checks if fired upon" do
+    @cell.place_ship(@cruiser)
+    expect(@cell.fired_upon?).to be false
+  end
 
-      it "checks if health -1 after being hit" do
-        @cell.place_ship(@cruiser)
-        @cell.fire_upon
-        expect(@cruiser.health).to eq(2)
-      end
+  it "checks if health -1 after being hit" do
+    @cell.place_ship(@cruiser)
+    @cell.fire_upon
+    expect(@cruiser.health).to eq(2)
+  end
 
-      it "after being hit return being fired upon true" do
-        @cell.place_ship(@cruiser)
-        @cell.fire_upon
-        expect(@cell.fired_upon?).to be true
-      end
+  it "after being hit return being fired upon true" do
+    @cell.place_ship(@cruiser)
+    @cell.fire_upon
+    expect(@cell.fired_upon?).to be true
+  end
 
-    before :each do
-        @cell_1 = Cell.new("B4")
-      end
-          it "renders an empty cell" do
-            expect(@cell_1.render).to eq(".")
-          end
+  before :each do
+    @cell_1 = Cell.new("B4")
+  end
+  it "renders an empty cell" do
+    expect(@cell_1.render).to eq(".")
+  end
 
-          it "gets fired upon and misses" do
-            @cell_1.fire_upon
-            expect(@cell_1.render).to eq("M")
-          end
+  it "gets fired upon and misses" do
+    @cell_1.fire_upon
+    expect(@cell_1.render).to eq("M")
+  end
 
-          it "renders another empty cell" do
-            @cell_2 = Cell.new("C3")
-            @cruiser = Ship.new("Cruiser", 3)
-            @cell_2.place_ship(@cruiser)
-            expect(@cell_2.render).to eq(".")
-          end
+  it "renders another empty cell" do
+    @cell_2 = Cell.new("C3")
+    @cruiser = Ship.new("Cruiser", 3)
+    @cell_2.place_ship(@cruiser)
+    expect(@cell_2.render).to eq(".")
+  end
 
-          it "shows an optional argument to #render if user" do
-            @cell_2 = Cell.new("C3")
-            @cruiser = Ship.new("Cruiser", 3)
-            @cell_2.place_ship(@cruiser)
-            expect(@cell_2.render(true)).to eq("S")
-          end
+  it "shows an optional argument to #render if user" do
+    @cell_2 = Cell.new("C3")
+    @cruiser = Ship.new("Cruiser", 3)
+    @cell_2.place_ship(@cruiser)
+    expect(@cell_2.render(true)).to eq("S")
+  end
 
-          it "returns H when #fire_upon is a hit" do
-            @cell_2 = Cell.new("C3")
-            @cruiser = Ship.new("Cruiser", 3)
-            @cell_2.place_ship(@cruiser)
-            @cell_2.fire_upon
-            expect(@cell_2.render).to eq("H")
-          end
+  it "returns H when #fire_upon is a hit" do
+    @cell_2 = Cell.new("C3")
+    @cruiser = Ship.new("Cruiser", 3)
+    @cell_2.place_ship(@cruiser)
+    @cell_2.fire_upon
+    expect(@cell_2.render).to eq("H")
+  end
 
-          it "checks if ship is #sunk?" do
-            @cell_2 = Cell.new("C3")
-            @cruiser = Ship.new("Cruiser", 3)
-            @cell_2.place_ship(@cruiser)
-            @cell_2.fire_upon
-            expect(@cruiser.sunk?).to be false
-          end
+  it "checks if ship is #sunk?" do
+    @cell_2 = Cell.new("C3")
+    @cruiser = Ship.new("Cruiser", 3)
+    @cell_2.place_ship(@cruiser)
+    @cell_2.fire_upon
+    expect(@cruiser.sunk?).to be false
+  end
 
-          it "sinks a ship" do
-            @cell_2 = Cell.new("C3")
-            @cruiser = Ship.new("Cruiser", 3)
-            @cell_2.place_ship(@cruiser)
-            @cell_2.fire_upon
-            @cruiser.hit
-            @cruiser.hit
-            expect(@cruiser.sunk?).to be true
-          end
-          it "returns X for a #sunk ship" do
-            @cell_2 = Cell.new("C3")
-            @cruiser = Ship.new("Cruiser", 3)
-            @cell_2.place_ship(@cruiser)
-            @cell_2.fire_upon
-            @cruiser.hit
-            @cruiser.hit
-            expect(@cell_2.render).to eq("X")
-          end
+  it "sinks a ship" do
+    @cell_2 = Cell.new("C3")
+    @cruiser = Ship.new("Cruiser", 3)
+    @cell_2.place_ship(@cruiser)
+    @cell_2.fire_upon
+    @cruiser.hit
+    @cruiser.hit
+    expect(@cruiser.sunk?).to be true
+  end
+  
+  it "returns X for a #sunk ship" do
+    @cell_2 = Cell.new("C3")
+    @cruiser = Ship.new("Cruiser", 3)
+    @cell_2.place_ship(@cruiser)
+    @cell_2.fire_upon
+    @cruiser.hit
+    @cruiser.hit
+    expect(@cell_2.render).to eq("X")
+  end
 end
